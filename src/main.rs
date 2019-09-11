2d6049a5a85dcbb32d5f4539200d47fe0431de3d
jrincon2 <julio.rincon@macquarie.com>
2019-09-11 17:12:21 +1000

    updated

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[derive(Debug)]
 struct Tag{
     name:String,
+    // props:HashMap<String,Any>,
     children: Vec<Tag>,
 }
 use std::collections::HashMap;
@@ -23,12 +24,29 @@ macro_rules! kvmap{
 }
 
 macro_rules! hiccup{
-    ($tag:ident) => {Tag{name: stringify!($tag).into(), children:vec!()}};
-    ($tag:ident $( { $($k:ident : $v:expr),* } )? $([$child:tt])*) => {
+    // ($tag:ident) => {Tag{name: stringify!($tag).into(), children:vec!()}};
+    (@empty ()) => {};
+    (@props $($k:ident : $v:expr),+) => {
         {
+            let mut m = HashMap::new();
+            $(m.insert(stringify!($k), $v);)+
+                m
+        }
+    };
+    ($tag:ident) => {hiccup!($tag {})};
+
+    ($tag:ident {$props:pat}) => {hiccup!($tag {$props})};
+
+    ($tag:ident $([$children:pat])+) => {hiccup!($tag {} $([$children])+)};
+
+    ($tag:ident {$($props:tt)*} $([$($child:tt)*])*) => {
+        {
+            // $(let props = hiccup!(@props $props)
+            //   println!("parsed props {:?}", props);)*;
+
             let mut v = Vec::new();
-            $(v.push(hiccup!($child));)*
-            Tag{name: stringify!($tag).into(), children:v}
+            $(v.push(hiccup!($($child)*));)*
+                Tag{name: stringify!($tag).into(), children:v}
         }
     };
 }
@@ -36,7 +54,7 @@ macro_rules! hiccup{
 fn main() {
     println!("Hello, world! {:?}", Tag{name:"div".into(), children:vec![Tag{name:"span".into(), children:vec![]}]});
     let t = hiccup![br];
-    let t2 = hiccup![div { prop: "a"} [span { x : "1" }] [a]];
+    let t2 = hiccup![div { prop : "a"} [span {x:"1"}] [a]];
     println!("Tag: {:?}", t2);
     let m = kvmap!{"1" : 2, "3" : 4, "abra cadabra": 5};
     println!("map1: {:?}, other, {:?}", m,  kvmap!{1:2, 3:4+5});
